463a61937966984d564e9949436227c8dbd1e3f9
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-08-02 19:07:18 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Connected to MongoDB Database'));
+
+app.use(express.json());
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
+
+app.listen(3000, () => console.log('Server Stared'));
